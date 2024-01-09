5d9563a49426e9250a288c7a6909b1ae33129c54
Bayola <nicoldcarcache@gmail.com>
2024-01-09 15:07:35 -0600

    hero component and home hero, done

---
+++ components/devCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    imageSrc: String,
+    name: String,
+    position: String,
+    description: String,
+    skills: String,
+  },
+};
+</script>
+
+<template>
+  <div class="devCard">
+    <NuxtImg :src="imageSrc" alt="Developer profile picture" class="devCard__image"/>
+    <h4 class="devCard__name">{{ name }}</h4>
+    <span class="devCard__position">{{ position }}</span>
+    <p class="devCard__description">{{ description }}</p>
+    <cta customClass="cta--type-outlined devCard__cta">Engage</cta>
+    <span class="devCard--skills devCard--skills-uppercase">Skills</span>
+    <p class="devCard--skills">{{ skills }}</p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,23 @@ import "assets/scss/main.scss";
 </script>
 <template>
   <main>
-    <h1>Welcome to the homepage</h1>
+    <hero
+      columnWide="ten wide computer sixteen wide mobile"
+      title="Boost your Software team with LATAM talent. "
+      subtitle="Staff augmentation made easy."
+      description="Whether you need one developer or an entire team, we have the expertise to find the right fit for your project. Plus, with our flexible engagement models, you can choose the level of support that works best for your needs."
+      cta="Get in Touch"
+      imageSrc="/clutch-batch.png"
+    >
+      <div class="six wide column computer only">
+        <devCard
+          imageSrc="/card-logo.png"
+          name="John Doe"
+          position="Front end developer"
+          description="Software Engineer with over 6 years of experience in Web development. I enjoy practicing photography."
+          skills="JavaScript, ReactJs, HTML, CSS"
+        />
+      </div>
+    </hero>
   </main>
 </template>
