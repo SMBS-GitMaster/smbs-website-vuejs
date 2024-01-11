721f092f603bcd3977226b450ce52eb3eab85209
Bayola <nicoldcarcache@gmail.com>
2024-01-11 10:38:18 -0600

    add marquee component and section, done

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <script>
 import "semantic-ui-css/semantic.css";
 import "assets/scss/main.scss";
+import ourStackImages from "~/data/ourStack.json";
+
+export default {
+  data() {
+    return {
+      ourStackImages,
+    };
+  },
+};
+
 </script>
 <template>
   <main>
-    <h1>Welcome to the homepage</h1>
+    <marqueeSection title="our stack" :images="ourStackImages" />
   </main>
 </template>
